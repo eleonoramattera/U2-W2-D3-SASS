d634b2deb8a8b1d5e1edc752134e505e50f2fc5c
Eleonora Mattera <m.eleonora98@live.it>
2023-01-21 17:40:12 +0100

    sass

---
+++ script.js
@@ -0,0 +1,21 @@
+function goPizza(event) {
+  document.querySelector(".goPizza").style.transform = "rotate(20deg)";
+  //document.querySelector("smile").style.transform = "rotate(20deg)";
+}
+
+//<button onclick="myFunction()">Click me</button>
+
+//<p id="demo"></p>
+
+//<script>
+//function myFunction() {
+//  document.getElementById("demo").innerHTML = "Hello World";
+//}
+//</script>
+
+//document.getElementById("myDIV").style.transform = "rotate(20deg)";
+
+
+
+function pink()
+document.querySelector(".pink").style.color="pink"
